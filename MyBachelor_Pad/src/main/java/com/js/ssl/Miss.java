fcd27284b7cdcbbe9a0debbe2617c03df8eec3b2
SongForest <sslxhhbr@163.com>
2019-01-28 16:23:11 +0800

    随便

---
+++ MyBachelor_Pad/src/main/java/com/js/ssl/Miss.java
@@ -0,0 +1,9 @@
+package com.js.ssl;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Miss {
+    public static void main(String[] args) {
+        //AnnotationConfigApplicationContext
+    }
+}
